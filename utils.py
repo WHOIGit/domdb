7fee8186626a0b52a43aa147d81a90e4f7201251
Joe Futrelle <joefutrelle@gmail.com>
2015-02-04 11:31:19 -0500

    snazzier version of asciitable with border

--- utils.py
+++ utils.py
@@ -1,21 +1,22 @@
 def rpad(s,l,pad_string=' '):
     return s + (pad_string * (l - len(s)))
 
-def asciitable(dicts,disp_cols=None,none_msg=None):
+def asciitable(dicts,disp_cols=None,none_msg=None,border=True):
     """produce an ASCII formatted columnar table from the dicts"""
     dicts = list(dicts)
     if not dicts:
         if none_msg is not None:
             yield none_msg
         return
-    # set of all keys in dicts
-    cols = sorted(list(set(reduce(lambda x,y: x+y, [d.keys() for d in dicts]))))
     if disp_cols is not None:
         cols = disp_cols
+    else:
+        # set of all keys in dicts
+        cols = sorted(list(set(reduce(lambda x,y: x+y, [d.keys() for d in dicts]))))
     # compute col widths. initially wide enough for the column label
     widths = dict([(col,len(col)) for col in cols])
     # now create rows, and in doing so compute max width of each column
-    for row in dicts:
+    for row in list(dicts):
         for col in cols:
             try:
                 width = len(str(row[col]))
@@ -23,8 +24,18 @@ def asciitable(dicts,disp_cols=None,none_msg=None):
                 width = 0
             if width > widths[col]:
                 widths[col] = width
+    def bord(line,border_char='|',pad_char=' '):
+        if border:
+            return border_char + pad_char + line + pad_char + border_char
+        else:
+            return line
     # now print rows
-    yield ' | '.join([rpad(col,widths[col]) for col in cols])
-    yield '-+-'.join(['-' * widths[col] for col in cols])
+    spacer = bord('-+-'.join(['-' * widths[col] for col in cols]),'+','-')
+    if border:
+        yield spacer
+    yield bord(' | '.join([rpad(col,widths[col]) for col in cols]),'|')
+    yield spacer
     for row in dicts:
-        yield ' | '.join([rpad(str(row[col]),widths[col]) for col in cols])
+        yield bord(' | '.join([rpad(str(row[col]),widths[col]) for col in cols]),'|')
+    if border:
+        yield spacer
