d9140011a658a3a26c8811cde28b6191f4c633b6
Joe Futrelle <joefutrelle@gmail.com>
2015-06-03 15:59:14 -0400

    get_psql_engine

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import sqlalchemy
 from sqlalchemy import func
 from sqlalchemy.orm import sessionmaker
 
+PSQL_URL='postgresql://domdb:domdb@localhost/domdb'
+
 def get_sqlite_engine(delete=True):
     # first, toast db
     DB_FILE = 'kuj2.db'
@@ -14,3 +16,6 @@ def get_sqlite_engine(delete=True):
             print 'unable to delete %s' % DB_FILE
             raise
     return sqlalchemy.create_engine('sqlite:///%s' % DB_FILE)
+
+def get_psql_engine():
+    return sqlalchemy.create_engine(PSQL_URL)
