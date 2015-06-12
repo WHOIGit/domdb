c8c5b11c263323422ad63fcf15b44818d84a2d3a
Joe Futrelle <joefutrelle@gmail.com>
2015-06-12 12:57:23 -0400

    refactoring in preparation of wiring into CLI

--- new_search.py
+++ new_search.py
@@ -19,46 +19,27 @@ def construct_search(mz,rt,ioc=None,ppm_diff=0.5,rt_range=30,attrs=None):
         params = (mz,ppm_diff,rt,rt_range)
     return query, params
 
-cases = [
-    dict(mz=100,rt=90),
-    dict(mz=102,rt=90),
-    dict(mz=102,rt=200),
-    dict(mz=103,rt=100),
-    dict(mz=105,rt=80,ioc=10),
-    dict(mz=107,rt=200,attrs=['media']),
-    dict(mz=108,rt=100,attrs=['media']),
-    dict(mz=109,rt=500,attrs=['media','time']),
-    dict(mz=111,rt=500,attrs=['media','time']),
-    dict(mz=113,rt=100,attrs=['media'],ioc=10),
-    dict(mz=113,rt=200,attrs=['media'],ioc=10),
-    dict(mz=114,rt=100,attrs=['media'],ioc=10),
-    dict(mz=116,rt=100,attrs=['media','time'],ioc=10)
-]
+def search(engine,mz,rt,ioc=None,ppm_diff=0.5,rt_range=30,attrs=None):
+    """returns ResultProxy"""
+    c = engine.connect()
+    query, params = construct_search(mz,rt,ioc=ioc,ppm_diff=ppm_diff,rt_range=rt_range,attrs=attrs)
+    return c.execute(query,*params)
 
-engine = get_psql_engine()
-c = engine.connect()
-
-for case in cases:
-    print case
-    query, params = construct_search(case.get('mz'),
-                                     case.get('rt'),
-                                     ioc=case.get('ioc'),
-                                     attrs=case.get('attrs',[]))
-    r = c.execute(query,*params)
+def results_as_csv(r):
     rows = r.fetchall()
+    cols = [x for x in r.keys() if x != 'attrs']
     if not rows:
-        continue
+        yield ','.join(cols)
+        return
     # determine column headings
-    cols = []
     attrs = []
     for row in rows:
-        cols = row.keys()
         for kv in dict(row.items()).get('attrs'):
             k, v = kv.split('=')
             if k not in attrs:
                 attrs += [k]
-    cols = [x for x in cols if x != 'attrs'] + attrs
-    print ','.join(cols)
+    cols += attrs
+    yield ','.join(cols)
     # now postprocess rows
     for row in rows:
         rd = dict(row.items())
@@ -66,4 +47,35 @@ for case in cases:
         ad = dict([kv.split('=') for kv in rd['attrs']])
         del rd['attrs']
         rd.update(ad) # FIXME avoid name collisions
-        print ','.join(str(rd[c]) for c in cols)
+        yield ','.join(str(rd[c]) for c in cols)
+
+def test(engine):
+    cases = [
+        dict(mz=100,rt=90),
+        dict(mz=102,rt=90),
+        dict(mz=102,rt=200),
+        dict(mz=103,rt=100),
+        dict(mz=105,rt=80,ioc=10),
+        dict(mz=107,rt=200,attrs=['media']),
+        dict(mz=108,rt=100,attrs=['media']),
+        dict(mz=109,rt=500,attrs=['media','time']),
+        dict(mz=111,rt=500,attrs=['media','time']),
+        dict(mz=113,rt=100,attrs=['media'],ioc=10),
+        dict(mz=113,rt=200,attrs=['media'],ioc=10),
+        dict(mz=114,rt=100,attrs=['media'],ioc=10),
+        dict(mz=116,rt=100,attrs=['media','time'],ioc=10)
+    ]
+    for case in cases:
+        print case
+        r = search(engine,
+                   case.get('mz'),
+                   case.get('rt'),
+                   ioc=case.get('ioc'),
+                   attrs=case.get('attrs',[]))
+        for line in results_as_csv(r):
+            print line
+
+if __name__=='__main__':
+    engine = get_psql_engine()
+    test(engine)
+
