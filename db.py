c772b7783b3f16317e5c6e3cd608283667bb30d0
klongnecker <klongnecker@whoi.edu>
2015-07-20 15:34:49 -0400

    Krista making generic notes re password and username.

--- db.py
+++ db.py
@@ -1,1 +1,4 @@
-DATABASE_URL='postgresql://username:password@host.whatever.edu/domdb'
+#change user to the user name on the data computer
+#change password to the correct password name
+#uncomment out the following line once you have the correct information
+#DATABASE_URL='postgresql://username:password@nier.whoi.edu/databaseName'
